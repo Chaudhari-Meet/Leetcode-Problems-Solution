dcd02fbff4df1e5287cb5e28fd6d03d0f06b5d58
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-22 14:23:24 +0530

    Time: 0 ms (100%), Space: 42.5 MB (42.42%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -4,9 +4,8 @@ class Solution {
         int sum=0;
         int product=1;
         while(x>0){
-            int rem = x%10;
-            sum+=rem;
-            product*=rem;
+            sum+=x%10;
+            product*=x%10;
             x=x/10;
         }
         return (n%(sum+product) == 0) ? true : false ; 
