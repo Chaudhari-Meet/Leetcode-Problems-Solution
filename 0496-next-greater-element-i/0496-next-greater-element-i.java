ee299100167c865aab05046102cbbc4f5d3822f5
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-25 20:01:31 +0530

    Time: 4 ms (74.12%), Space: 45.4 MB (22.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n = nums2.length;
-        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Map<Integer, Integer> nextMap = new HashMap<>();
         Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+        nextMap.put(nums2[n-1],-1);
+        stack.push(nums2[n-1]);
+        for(int i=n-2;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums2[i]){
                 stack.pop();
             }
-            if (stack.isEmpty()) {
-                nextGreaterMap.put(nums2[i], -1);
-            } else {
-                nextGreaterMap.put(nums2[i], stack.peek());
+            if(stack.isEmpty()){
+                nextMap.put(nums2[i],-1);
+            }else{
+                nextMap.put(nums2[i],stack.peek());
             }
             stack.push(nums2[i]);
         }
-        int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = nextGreaterMap.get(nums1[i]);
+        for(int i=0;i<nums1.length;i++){
+            int val = nextMap.get(nums1[i]);
+            nums1[i]=val;
         }
-        return result;
+        return nums1;
     }
 }
