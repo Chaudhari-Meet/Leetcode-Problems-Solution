3d52d5de6227587a1c1c3fe1eaa8d7d4717aac9a
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-25 15:32:40 +0530

    Time: 6 ms (87.19%), Space: 45.2 MB (78.66%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -3,25 +3,24 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         int result;
         for(int i=0;i<tokens.length;i++){
-            String token = tokens[i];
-            if(token.equals("+")){
+            if(tokens[i].equals("+")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b+a);
-            }else if(token.equals("-")){
+            }else if(tokens[i].equals("-")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b-a);
-            }else if(token.equals("*")){
+            }else if(tokens[i].equals("*")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b*a);
-            }else if(token.equals("/")){
+            }else if(tokens[i].equals("/")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b/a);
             }else{
-                stack.push(Integer.parseInt(token));
+                stack.push(Integer.parseInt(tokens[i]));
             }
         }
         return stack.peek();
