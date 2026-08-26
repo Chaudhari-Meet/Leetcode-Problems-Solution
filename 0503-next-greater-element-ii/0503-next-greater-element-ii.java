59e2138ff500b4454b03234133ec5f6bdb89d1c4
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-26 14:29:41 +0530

    Time: 1 ms (100%), Space: 47.8 MB (87.51%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
         int[] result = new int[n];
-        for(int i=0;i<n;i++){
-            int start_index=(i+1)%n;
-            result[i]=-1;
-            while(start_index!=i){
-                if(nums[i]<nums[start_index]){
-                    result[i]=nums[start_index];
-                    break;
-                }else{
-                    start_index = (start_index + 1) % n;
-                }
+        for (int i = 0; i < n; i++) {
+            result[i] = -1;
+        }
+        int[] stack = new int[n];
+        int top = -1;
+        for (int i = 0; i < 2 * n; i++) {
+            int index = i % n;
+            while (top >= 0 && nums[stack[top]] < nums[index]) {
+                result[stack[top--]] = nums[index];
+            }
+            if (i < n) {
+                stack[++top] = index;
             }
         }
         return result;
