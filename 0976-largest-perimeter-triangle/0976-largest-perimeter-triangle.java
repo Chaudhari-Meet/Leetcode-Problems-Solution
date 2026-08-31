84c92678c21e002877460bc5d2420aaa1673fa7c
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-31 15:37:10 +0530

    Time: 133 ms (5.68%), Space: 47.5 MB (79.12%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.java
@@ -5,9 +5,7 @@ class Solution {
         int n = nums.length;
         for(int i = n-1;i>=2;i--){
             if(nums[i-1] + nums[i-2] > nums[i]){
-
                 return nums[i-2] + nums[i-1] + nums[i];
-
             }
         }
         return 0;
