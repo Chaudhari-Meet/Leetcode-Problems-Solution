2a42af41467db5f6924e51216555a4713a3e7400
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-08-31 14:50:33 +0530

    Time: 55 ms (12.3%), Space: 115.7 MB (57.64%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -8,13 +8,12 @@ class Solution {
         int c=1;
 
         int k=0;
-        
+
         for(int i=1;i<intervals.length;i++){
 
             if(intervals[i][0] >= intervals[k][1]){
 
                 c++;
-
                 k=i;
 
             }
