ec89f7246352f88fa272c2485d1093d321ea298b
Chaudhari_Meet_17 <meetdchaudhari2006@gmail.com>
2026-09-01 14:02:10 +0530

    Time: 2 ms (73.25%), Space: 42.8 MB (45.28%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -10,7 +10,9 @@ class Solution {
         int small = 0;
         int large = 0;
         while (small < s.length() && large < t.length()) {
-            if (s.charAt(small) == t.charAt(large)) {
+            if(small == s.length()){
+                return true;
+            }else if (s.charAt(small) == t.charAt(large)) {
                 small++;
             }
             large++;
